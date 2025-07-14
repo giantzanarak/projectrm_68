d53fd9ab602bc64836a2254aa0ad866295c3d819
giantzanarak <pawat.wo@kkumail.com>
2025-07-14 19:29:15 +0700

    eeee

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>ohm</h1>
-      <h1>ohmeieieieieieiei</h1>
+      <h1>giantppxp</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
